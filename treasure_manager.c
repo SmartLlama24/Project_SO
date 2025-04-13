2e92584b684695d91f23298b7e1757e630e9ec19
SmartLlama24 <ro.thomas2405@gmail.com>
2025-04-13 17:09:54 +0300

    added list

--- treasure_manager.c
+++ treasure_manager.c
@@ -3,6 +3,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
 
 typedef struct treasure{
     char treasureID[11];
@@ -22,24 +24,46 @@ void add(char huntID[7]){
     }while(sscanf(buff,"%s %s %f %f %s %d",&t.treasureID,&t.username,&t.lat,&t.lng,&t.clue,&t.value) != 6);
 
     char dirPath[13] = "Hunts/";
-    char filePath[27];
-    strcat(dirPath,huntID);
-    strcpy(filePath,dirPath);
-    strcat(filePath,"/Treasures.txt");
+    char filePath[27] = "";
 
+    strcat(dirPath,huntID);
     mkdir(dirPath,0777);
-    int file = open(filePath, O_WRONLY | O_CREAT);
+
+    strcpy(filePath,dirPath);
+    strcat(filePath,"/Treasures.bin");
+
+    int file = open(filePath, O_WRONLY | O_APPEND | O_CREAT, 0666);
 
     write(file,&t,sizeof(t));
 
     close(file);
 }
 
+void list(char huntID[7]){
+    char filePath[27] = "Hunts/";
+    struct stat st;
+
+    strcat(filePath,huntID);
+    strcat(filePath,"/Treasures.bin");
+
+    int file = open(filePath, O_RDONLY);
+    stat(filePath,&st);
+
+    printf("\n%s\nSize: %ld bytes\nLast modified: %s\n",huntID,st.st_size,ctime(&st.st_mtime));
+
+    treasure t;
+
+    while(read(file,&t,sizeof(treasure)) != 0){
+        printf("%s %f %f %s %d\n",t.treasureID,t.lat,t.lng,t.clue,t.value);
+    }
+}
+
 int main(int argc, char* argv[]){
-    int result = mkdir("Hunts",0777);
+    int result = mkdir("Hunts",0666);
 
     if(result == -1) printf("Hunts directory already exists\n");
     else printf("Created Hunts directory\n");
 
-    add("Hunt001");
+    //add("Hunt001");
+    list("Hunt001");
 }
